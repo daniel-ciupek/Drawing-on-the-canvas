377025f8797b23c5982bf6a52f57554fd4a9c792
daniel-ciupek <danter005@o2.pl>
2025-11-09 15:39:19 +0100

    onnecting the drawing panel (drawing.js) to index.js

--- drawing.js
+++ drawing.js
@@ -1,16 +1,22 @@
-export default class Panel {
+export default class DrawingPanel {
   constructor() {
-    console.log("DrawingPanel initialized");
-  }
-};
-
-class DrawingPanel {
-constructor() {
     this.picker = document.querySelector(".color-picker");
     this.widthRange = document.querySelector(".line-range");
     this.widthLabel = document.querySelector(".range-value");
+  }
 
+  addPanelListeners = (colorCallback, widthCallback) => {
+    if (this.picker) {
+      this.picker.addEventListener("change", (e) => {
+        colorCallback(e.target.value);
+      });
+    }
+    if (this.widthRange) {
+      this.widthRange.addEventListener("input", (e) => {
+        const width = e.target.value;
+        if (this.widthLabel) this.widthLabel.innerHTML = ` ${width}px`;
+        widthCallback(width);
+      });
+    }
+  };
 }
-}
-
-const drawingPanel = new DrawingPanel();
--- index.js
+++ index.js
@@ -2,65 +2,60 @@ import DrawingPanel from "./drawing.js";
 
 const drawingPanel = new DrawingPanel();
 
-
 class CanvasDrawing {
-constructor(drawingPanel){
-this.canvas = document.querySelector(".canvas");
-this.canvas.width = window.innerWidth;
-this.canvas.height = window.innerHeight;
+  constructor(drawingPanel) {
+    this.canvas = document.querySelector(".canvas");
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.context = this.canvas.getContext("2d");
+    this.context.lineCap = "round";
+    this.context.lineJoin = "round";
+    this.setLineWidth(1);
+    this.setLineColor("#000");
+    this.drawing = false;
+    this.drawingPanel = drawingPanel;
+    this.init();
+  }
 
-this.context = this.canvas.getContext("2d");
-this.context.lineCap = "round";
-this.context.lineJoin = "round";
-this.setLineWidth(1);
-this.setLineColor("#000");
+  setLineWidth = (width) => {
+    this.context.lineWidth = width;
+  };
 
-this.drawing = false;
-this.drawingPanel = drawingPanel;
-
-this.init();
-}
-
-setLineWidth(width){
-    this.context.setLineWidth = width;
-}
-
-setLineColor(color){
+  setLineColor = (color) => {
     this.context.strokeStyle = color;
+  };
+
+  init() {
+    this.addListeners();
+  }
+
+  addListeners() {
+    const canvas = this.canvas;
+    canvas.addEventListener("mousedown", this.startDrawing);
+    canvas.addEventListener("mousemove", this.draw);
+    canvas.addEventListener("mouseup", this.stopDrawing);
+    canvas.addEventListener("mouseout", this.stopDrawing);
+    this.drawingPanel.addPanelListeners(this.setLineColor, this.setLineWidth);
+  }
+
+  startDrawing = (e) => {
+    this.drawing = true;
+    this.lastX = e.offsetX;
+    this.lastY = e.offsetY;
+  };
+
+  stopDrawing = () => {
+    this.drawing = false;
+  };
+
+  draw = (e) => {
+    if (!this.drawing) return;
+    this.context.beginPath();
+    this.context.moveTo(this.lastX, this.lastY);
+    this.context.lineTo(e.offsetX, e.offsetY);
+    this.context.stroke();
+    [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
+  };
 }
 
-
-init() {
-this.addListeners();
-}
-addListeners(){
-const canvas = this.canvas;
-
-canvas.addEventListener("mousedown", this.startDrawing);
-canvas.addEventListener("mousemove", this.draw);
-canvas.addEventListener("mouseup", this.stopDrawing);
-canvas.addEventListener("mouseout", this.stopDrawing);
-
-}
-startDrawing = (e) => {
-this.drawing = true;
-this.lastX = e.offsetX;
-this.lastY = e.offsetY;
-}
-
-stopDrawing = (e) => {
-this.drawing = false;
-}
-
-draw = (e) => {
-if (!this.drawing) return;
-const context = this.context;
-context.beginPath();
-context.moveTo(this.lastX, this.lastY)
-context.lineTo(e.offsetX, e.offsetY);
-context.stroke();
-[this.lastX, this.lastY ] = [e.offsetX, e.offsetY];
-}
-}
 const canvasDrawing = new CanvasDrawing(drawingPanel);
-
